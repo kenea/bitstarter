69c52d0f9f473728decd76e2046bb220f077d38d
kenea <kmaraffio@gmail.com>
2013-07-08 21:23:14 +0000

    Modified to read url

--- grader.js
+++ grader.js
@@ -21,11 +21,14 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
-var fs = require('fs');
-var program = require('commander');
-var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var fs                 = require('fs');
+var program            = require('commander');
+var cheerio            = require('cheerio');
+var restler            = require('restler');
+var util               = require('util');
+var HTMLFILE_DEFAULT   = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT        = 'https://spark-public.s3.amazonaws.com/startup/code/bitstarter.html';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +38,21 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+
+var assertUrlExists = function(inurl) {
+  restler.get(inurl).on('complete', function(result, response)
+    {
+    if (result instanceof Error) 
+      {
+      console.log('Error: ' + util.format(response.message));
+      process.exit(1);
+      }
+    else 
+      return response;
+    });
+};
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,8 +83,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <application_url>', 'URL to application', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var checkJson;
+    if (program.url)
+      checkJson = checkHtmlFile(program.url, CHECKSFILE_DEFAULT);
+    else
+      checkJson = checkHtmlFile(program.file, program.checks);
+  
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
